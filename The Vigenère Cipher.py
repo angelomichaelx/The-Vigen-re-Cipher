3af7fd4f768beec71c7f802fd05516e7a8adf053
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-04-21 18:23:17 +0800

    final program

--- The Vigenère Cipher.py
+++ The Vigenère Cipher.py
@@ -4,26 +4,26 @@
 def main():
     #implementing keypadding method
     def _pad_key(plaintext, key):
-            padded_key = ''
-            i = 0
+        padded_key = ''
+        i = 0
 
     #loop over the characters of the plain text
-    for char in plaintext:
-                if char.isalpha():
-                    padded_key += key[i % len(key)]
-                    i += 1
-                else:
-                    padded_key += ' '
-    return padded_key
+        for char in plaintext:
+            if char.isalpha():
+                padded_key += key[i % len(key)]
+                i += 1
+            else:
+                padded_key += ' '
+        return padded_key
 
     #implementing the encrtypt and decrypt method
     def _encrypt_decrypt_char(plaintext_char, key_char, mode='encrypt'):
-            if plaintext_char.isalpha():
-                if plaintext_char.isupper():
-                    first_alphabet_letter = 'A'
-                    
+        if plaintext_char.isalpha():
+            if plaintext_char.isupper():
+                first_alphabet_letter = 'A'
+
     #subctracting the position of the first alphabet letter from the position of the plain text character
-                old_char_position = ord(plaintext_char) - ord(first_alphabet_letter)
+            old_char_position = ord(plaintext_char) - ord(first_alphabet_letter)
             key_char_position = ord(key_char.lower()) - ord('a')
 
             if mode == 'encrypt':
@@ -31,10 +31,10 @@ def main():
             else:
                 new_char_position = (old_char_position - key_char_position + 26) % 26
             return chr(new_char_position + ord(first_alphabet_letter))
-    return plaintext_char
+        return plaintext_char
 
     #encrypt method
-def encrypt(plaintext, key):
+    def encrypt(plaintext, key):
         ciphertext = ''
         padded_key = _pad_key(plaintext, key)
         for plaintext_char, key_char in zip(plaintext, padded_key):
@@ -42,31 +42,31 @@ def encrypt(plaintext, key):
         return ciphertext
 
     #decrypt method
-def decrypt(ciphertext, key):
+    def decrypt(ciphertext, key):
         plaintext = ''
         padded_key = _pad_key(ciphertext, key)
         for ciphertext_char, key_char in zip(ciphertext, padded_key):
             plaintext += _encrypt_decrypt_char(ciphertext_char, key_char, mode='decrypt')
-        return plaintext    
+        return plaintext
 
     #Ask the user to input a message and key
-plain_text = input('Enter a message (all uppercase letters, no spaces): ')
-key = input('Enter a key (all uppercase letters): ')
+    plain_text = input('Enter a message (all uppercase letters, no spaces): ')
+    key = input('Enter a key (all uppercase letters): ')
 
-ciphertext = encrypt(plain_text, key)
-decrypted_plaintext = decrypt(ciphertext, key)
+    ciphertext = encrypt(plain_text, key)
+    decrypted_plaintext = decrypt(ciphertext, key)
 
     #print output with decor
-print("\033[1;36m" +"=" * 20)
-print(f'Encrypted message: {ciphertext}')
-print(f'Decrypted message: {decrypted_plaintext}')
-print("=" * 20)
-
-    #program if you want to repeat it again
-Repeat = input("Would you like to try again? (YES/NO) : ").upper()
-if Repeat == "YES":
+    print("\033[1;36m" +"=" * 20)
+    print(f'Encrypted message: {ciphertext}')
+    print(f'Decrypted message: {decrypted_plaintext}')
+    print("=" * 20)
+    
+ #program if you want to repeat it again
+    Repeat = input("Would you like to try again? (YES/NO) : ").upper()
+    if Repeat == "YES":
             main()
-else:
+    else:
             print("Thank you!!")
             exit()
 main()
