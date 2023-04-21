bd8e60aee89d56a7cc1c5757fc313a2f340fd4df
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-04-21 18:14:12 +0800

    implementing the encrtypt and decrypt method

--- The Vigenère Cipher.py
+++ The Vigenère Cipher.py
@@ -6,6 +6,7 @@ def main():
     def _pad_key(plaintext, key):
         padded_key = ''
         i = 0
+
 #loop over the characters of the plain text
 for char in plaintext:
             if char.isalpha():
@@ -14,7 +15,13 @@ for char in plaintext:
             else:
                 padded_key += ' '
         return padded_key
+
 #implementing the encrtypt and decrypt method
+    def _encrypt_decrypt_char(plaintext_char, key_char, mode='encrypt'):
+        if plaintext_char.isalpha():
+            if plaintext_char.isupper():
+                first_alphabet_letter = 'A'
+                
 #subctracting the position of the first alphabet letter from the position of the plain text character
 #encrypt method
 #decrypt method
