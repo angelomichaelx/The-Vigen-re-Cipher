e102a024f0640eb8a11b6d71ed4e1e8886bb41dc
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-04-21 18:16:10 +0800

    subctracting the position of the first alphabet letter from the position of the plain text character

--- The Vigenère Cipher.py
+++ The Vigenère Cipher.py
@@ -2,29 +2,39 @@
 #BSCOE 1-4
 #Problem number 3 The Vigenère Cipher  (50 points)
 def main():
-#implementing keypadding method
+    #implementing keypadding method
     def _pad_key(plaintext, key):
-        padded_key = ''
-        i = 0
+            padded_key = ''
+            i = 0
 
-#loop over the characters of the plain text
-for char in plaintext:
-            if char.isalpha():
-                padded_key += key[i % len(key)]
-                i += 1
-            else:
-                padded_key += ' '
-        return padded_key
+    #loop over the characters of the plain text
+    for char in plaintext:
+                if char.isalpha():
+                    padded_key += key[i % len(key)]
+                    i += 1
+                else:
+                    padded_key += ' '
+    return padded_key
 
-#implementing the encrtypt and decrypt method
+    #implementing the encrtypt and decrypt method
     def _encrypt_decrypt_char(plaintext_char, key_char, mode='encrypt'):
-        if plaintext_char.isalpha():
-            if plaintext_char.isupper():
-                first_alphabet_letter = 'A'
-                
-#subctracting the position of the first alphabet letter from the position of the plain text character
-#encrypt method
-#decrypt method
-#Ask the user to input a message and key
-#print output with decor
-#program if you want to repeat it again
+            if plaintext_char.isalpha():
+                if plaintext_char.isupper():
+                    first_alphabet_letter = 'A'
+                    
+    #subctracting the position of the first alphabet letter from the position of the plain text character
+                old_char_position = ord(plaintext_char) - ord(first_alphabet_letter)
+            key_char_position = ord(key_char.lower()) - ord('a')
+
+            if mode == 'encrypt':
+                new_char_position = (old_char_position + key_char_position) % 26
+            else:
+                new_char_position = (old_char_position - key_char_position + 26) % 26
+            return chr(new_char_position + ord(first_alphabet_letter))
+    return plaintext_char
+
+    #encrypt method
+    #decrypt method
+    #Ask the user to input a message and key
+    #print output with decor
+    #program if you want to repeat it again
