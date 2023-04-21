7a4399dcc8f0f93e1fc0f3a0f1f08ad0fd015475
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-04-21 18:12:08 +0800

    implemented keypadding method

---
+++ The Vigenère Cipher.py
@@ -0,0 +1,16 @@
+#Michael Angelo P. Biag
+#BSCOE 1-4
+#Problem number 3 The Vigenère Cipher  (50 points)
+def main():
+#implementing keypadding method
+    def _pad_key(plaintext, key):
+        padded_key = ''
+        i = 0
+#loop over the characters of the plain text
+#implementing the encrtypt and decrypt method
+#subctracting the position of the first alphabet letter from the position of the plain text character
+#encrypt method
+#decrypt method
+#Ask the user to input a message and key
+#print output with decor
+#program if you want to repeat it again
